252fa87334c14c6da32180677a5cc26f272716e3
sword_chiu <53749772+18888628835@users.noreply.github.com>
2021-05-23 22:28:51 +0800

    add description 添加备注

--- src/store/Selector.ts
+++ src/store/Selector.ts
@@ -1,10 +1,11 @@
 import { todoListState, todoListFilterState } from "./Atom";
 import { selector } from "recoil";
-//这个文件用来放派生数据
+//这个文件用来放派生数据，派生数据就是用仓库数据进行某些操作而得出的数据
 //派生数据，可以获取到仓库中的多个数据，实现派生
 export const filteredTodoListState = selector({
   key: "filteredTodoListState",
   get: ({ get }) => {
+    //这里的派生数据主要是根据 filter 值和 todoList 值进行过滤数据
     const filter = get(todoListFilterState);
     const list = get(todoListState);
 
